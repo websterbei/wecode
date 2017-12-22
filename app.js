8113e745f1508ed0f9cc681869f41398fec783ed
websterbei <bei.yijie.2014@vjc.sg>
2017-12-22 18:58:32 +0800

    wecode

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = 3000;
+var port = 443;
 var server = https.createServer(options, app).listen(port, function(){
   console.log("Express server listening on port " + port);
 });
